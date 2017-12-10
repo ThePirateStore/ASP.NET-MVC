a029ef47fccf524b649c80d780da33ead31bf98b
Nishan Kumar Aryal <aryalnishan@outlook.com>
2017-12-11 05:27:58 +0545

    Initial Add : ManageUserController with UserWithRoles Method

---
+++ MVC5Demo.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5Demo.UI.MvcApplication" Language="C#" %>
---
+++ MVC5Demo.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Demo.UI.Startup))]
+namespace MVC5Demo.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
